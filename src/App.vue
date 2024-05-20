2dc1c92b88725078c3d37c61150cf7295b7b3765
z-anah <40556052+z-anah@users.noreply.github.com>
2024-05-20 14:55:06 +0700

    clean

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
